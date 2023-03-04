40405c158124b5d178bc24bf1cc7ce73a9f6e0d5
fero1xd <pranjalbutola12@gmail.com>
2023-03-04 13:55:50 +0530

    i am dumb

--- lib/src/main/java/me/fero/entities/ServerStatus.java
+++ lib/src/main/java/me/fero/entities/ServerStatus.java
@@ -3,13 +3,4 @@ package me.fero.entities;
 import org.json.simple.JSONObject;
 
 public record ServerStatus(JSONObject[] maintenances, JSONObject[] incidents) {
-    @Override
-    public JSONObject[] maintenances() {
-        return maintenances;
-    }
-
-    @Override
-    public JSONObject[] incidents() {
-        return incidents;
-    }
 }
--- lib/src/main/java/me/fero/entities/account/Card.java
+++ lib/src/main/java/me/fero/entities/account/Card.java
@@ -1,24 +1,4 @@
 package me.fero.entities.account;
 
 public record Card(String small, String large, String wide, String id) {
-
-    @Override
-    public String small() {
-        return small;
-    }
-
-    @Override
-    public String large() {
-        return large;
-    }
-
-    @Override
-    public String wide() {
-        return wide;
-    }
-
-    @Override
-    public String id() {
-        return id;
-    }
 }
